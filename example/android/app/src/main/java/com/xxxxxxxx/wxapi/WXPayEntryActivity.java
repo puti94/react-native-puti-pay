b01ae3885f59f25742620acef0df7e2078b015ba
putixie <1059592160@qq.com>
2017-12-19 18:22:42 +0800

    add example

---
+++ example/android/app/src/main/java/com/xxxxxxxx/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,11 @@
+package com.tiantiankanjia.wxapi;
+
+import com.puti.paylib.XWXPayEntryActivity;
+
+/**
+ * Created by puti on 2017/11/23.
+ */
+
+public class WXPayEntryActivity extends XWXPayEntryActivity {
+
+}
